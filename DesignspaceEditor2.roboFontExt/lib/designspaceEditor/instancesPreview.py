df21d0f514fbd12b2d66a420affa6f14a28c3b2e
Frederik Berlaen <frederik@typemytype.com>
2023-08-17 00:21:59 +0200

    improve switching between single and multi line for the instance preview

--- DesignspaceEditor2.roboFontExt/lib/designspaceEditor/instancesPreview.py
+++ DesignspaceEditor2.roboFontExt/lib/designspaceEditor/instancesPreview.py
@@ -5,10 +5,6 @@ from mojo.subscriber import WindowController, Subscriber, registerRoboFontSubscr
 from mojo.roboFont import RFont, RGlyph
 
 
-#import importlib
-#import ufoProcessor
-#importlib.reload(ufoProcessor)
-
 class InstancesPreview(Subscriber, WindowController):
 
     debug = True
@@ -21,7 +17,7 @@ class InstancesPreview(Subscriber, WindowController):
         self.w.input = vanilla.EditText((10, 10, -120, 22), callback=self.inputCallback)
         self.w.singleLine = vanilla.CheckBox((-100, 10, 100, 22), "Single line", value=True, callback=self.singleLineCheckboxCallback)
         self.w.hl = vanilla.HorizontalLine((0, 41, 0, 1))
-        self.w.preview = MultiLineView((0, 42, 0, 0), pointSize=60, displayOptions=dict(Beam=False))
+        self.w.preview = MultiLineView((0, 42, 0, 0), pointSize=60, displayOptions=dict(Beam=False, displayMode="Multi Line", Stroke=False, Fill=True))
         self.w.preview.setFont(dummyFont)
 
     def started(self):
@@ -29,10 +25,8 @@ class InstancesPreview(Subscriber, WindowController):
 
     def inputCallback(self, sender):
         glyphNames = splitText(sender.get(), self.operator.getCharacterMapping())
-        #print("glyphNames", glyphNames)
         glyphs = []
         for instance in self.operator.instances:
-            #print('instancesPreviewer', instance.location)
             for glyphName in glyphNames:
                 # do not bend, reasoning: the instance locations are in designspace values.
                 glyph = self.operator.makeOneGlyph(glyphName, instance.location, decomposeComponents=True)
@@ -41,14 +35,16 @@ class InstancesPreview(Subscriber, WindowController):
                     dest.fromMathGlyph(glyph)
                     dest.name = glyph.name
                     glyphs.append(dest)
-            if self.w.singleLine.get():
-                glyphs.append(self.w.preview.createNewLineGlyph())
+            glyphs.append(self.w.preview.createNewLineGlyph())
         self.w.preview.set(glyphs)
 
     designspaceEditorInstancesDidChangeDelay = 0.1
 
     def singleLineCheckboxCallback(self, sender):
-        self.inputCallback(self.w.input)
+        if sender.get():
+            self.w.preview.setDisplayStates(dict(displayMode="Multi Line"))
+        else:
+            self.w.preview.setDisplayStates(dict(displayMode="Single Line"))
 
     def designspaceEditorInstancesDidChange(self, notification):
         self.inputCallback(self.w.input)
