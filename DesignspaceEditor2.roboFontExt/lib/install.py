2589b984c4c0bf21768f432140f63411aba38050
Ryan Bugden <rbugden@gmail.com>
2025-11-17 16:13:12 -0500

    Close sheet after button press
    And clean up some print statements, local variables

--- DesignspaceEditor2.roboFontExt/lib/install.py
+++ DesignspaceEditor2.roboFontExt/lib/install.py
@@ -358,9 +358,11 @@ class NoDesignspaceFoundSheetController(ezui.WindowController):
         if paths:
             for path in paths:
                 OpenDesignspace(path)
+        self.w.close()
             
     def newDesignspaceButtonCallback(self, sender):
         NewDesignspace()
+        self.w.close()
 
 
 
@@ -487,11 +489,11 @@ class DesignspaceFontToolbarSubscriber(Subscriber):
         # toolbar = sender.toolbar()
         # window = toolbar._window()  # Gets a DoodleNSWindow, not a DoodleFontWindow :(
         # # We have to assume the button being clicked is on the active window
-        self.fw = CurrentFontWindow()
-        self.f = self.fw._font
-        if self.f is not None and self.f.path is not None:
-            recent = designspaceFinder.findRecentDesignspaces(self.f.path, verbose=True)
-            nearby = designspaceFinder.findNearbyDesignspaces(self.f.path, verbose=True)
+        fw = CurrentFontWindow()
+        f = fw._font
+        if f is not None and f.path is not None:
+            recent = designspaceFinder.findRecentDesignspaces(f.path, verbose=False)
+            nearby = designspaceFinder.findNearbyDesignspaces(f.path, verbose=False)
             
             # nearby results first
             #results = nearby + [d for d in recent if d not in nearby]
@@ -499,21 +501,22 @@ class DesignspaceFontToolbarSubscriber(Subscriber):
             results = recent + [d for d in nearby if d not in recent]
             
             if len(results) == 1:
-                print(f"action: opening 1 result: {results[0]}")
+                # print(f"action: opening 1 result: {results[0]}")
                 doc = None
                 try:
                     doc = OpenDesignspace(results[0])
                 except AttributeError:
-                    print('(DSE issue opening the same doc twice)')
+                    # print('(DSE issue opening the same doc twice)')
+                    pass
             elif len(results) > 1:
-                print(f"action: show dialog for {len(results)} files")
-                MultipleDesignspacesFoundSheetController(self.fw, results)
-                for p in results:
-                    print(f"\t\t{p}")
+                # print(f"action: show dialog for {len(results)} files")
+                MultipleDesignspacesFoundSheetController(fw, results)
+                # for p in results:
+                #     print(f"\t\t{p}")
             else:
-                NoDesignspaceFoundSheetController(self.fw)
+                NoDesignspaceFoundSheetController(fw)
         else:
-            NoDesignspaceFoundSheetController(self.fw)
+            NoDesignspaceFoundSheetController(fw)
 
 
 
