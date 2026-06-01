56d525bfba0f1f286ba5cf02af501b99085db1fd
Frederik Berlaen <frederik@typemytype.com>
2026-06-01 18:47:06 +0200

    Improve labeling with negative values

--- DesignspaceEditor2.roboFontExt/lib/designspaceEditor/parsers/labelsParser.py
+++ DesignspaceEditor2.roboFontExt/lib/designspaceEditor/parsers/labelsParser.py
@@ -38,10 +38,10 @@ from fontTools import designspaceLib
 from .parserTools import getLines, getBlocks, stringToNumber, numberToString
 
 
-axisLabelRE = re.compile(r"[\"|\']([a-zA-Z0-9\- ]+)[\"|\']\s*([0-9\.]*)\s*([0-9\.]*)\s*([0-9\.]*)")
-axisLabelLinkedUserValueRE = re.compile(r".*\[([0-9\.]+)\]")
+axisLabelRE = re.compile(r"[\"|\']([a-zA-Z0-9\- ]+)[\"|\']\s*([0-9\.\-]*)\s*([0-9\.\-]*)\s*([0-9\.\-]*)")
+axisLabelLinkedUserValueRE = re.compile(r".*\[([0-9\.\-]+)\]")
 labelNameRE = re.compile(r"\?\s+([a-zA-Z\-]+)\s+[\"|\'](.*)[\"|\']")
-userLocationRE = re.compile(r"([a-zA-Z\-\s]+)\s+([0-9\.]+)")
+userLocationRE = re.compile(r"([a-zA-Z\-\s]+)\s+([0-9\.\-]+)")
 
 defaultLabelNameLanguageTag = "en"
 
@@ -197,6 +197,7 @@ def storeLocationLabels(text, operator):
 
 @pytest.mark.parametrize("text,expected", [
     ('"Bold" 200 200 250', {'userMinimum': 200, 'userValue': 200, 'userMaximum': 250, 'name': 'Bold', 'elidable': False, 'olderSibling': False, 'linkedUserValue': None, 'labelNames': {}}),
+    ('"Bold" -200 -200 -150', {'userMinimum': -200, 'userValue': -200, 'userMaximum': -150, 'name': 'Bold', 'elidable': False, 'olderSibling': False, 'linkedUserValue': None, 'labelNames': {}}),
     ('"Extra Light" 200 200 250 (elidable) (olderSibling) [300]', {'userMinimum': 200, 'userValue': 200, 'userMaximum': 250, 'name': 'Extra Light', 'elidable': True, 'olderSibling': True, 'linkedUserValue': 300.0, 'labelNames': {}}),
     ('"Extra Light" 200 200 250 (elidable) [300] (olderSibling)', {'userMinimum': 200, 'userValue': 200, 'userMaximum': 250, 'name': 'Extra Light', 'elidable': True, 'olderSibling': True, 'linkedUserValue': 300.0, 'labelNames': {}}),
     ('"Extra Light" 200 200 250 [300] (elidable) (olderSibling)', {'userMinimum': 200, 'userValue': 200, 'userMaximum': 250, 'name': 'Extra Light', 'elidable': True, 'olderSibling': True, 'linkedUserValue': 300.0, 'labelNames': {}}),
